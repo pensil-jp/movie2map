127a7f6b9719b54b674cfcc5d66ed3e2ec998302
pensil <ohol@pensil.jp>
2019-03-02 22:42:24 +0900

    文字コード変更 UTF-8 → Shift-JIS

--- movie2map.bat
+++ movie2map.bat
@@ -1,22 +1,22 @@
 @ECHO OFF
 SETLOCAL
 
-@ECHO movie2map - ���悩��}�b�v���쐬����c�[�� ver0.1 by pensil 2019.02.26
+@ECHO movie2map - 動画からマップを作成するツール ver0.1 by pensil 2019.02.26
 @ECHO.
-@REM (�g����)
-@REM ffmpeg.exe�Ƀp�X��ʂ����A���̊��ϐ� FFMPEG �ɃR�}���h�̃t���p�X���L�ڂ��Ă�������
-@REM python.exe���p�X��ʂ����A���̊��ϐ� PYTHON �Ƀt���p�X���L�ڂ��Ă�������
+@REM (使い方)
+@REM ffmpeg.exeにパスを通すか、下の環境変数 FFMPEG にコマンドのフルパスを記載してください
+@REM python.exeもパスを通すか、下の環境変数 PYTHON にフルパスを記載してください
 
 set FFMPEG=%USERPROFILE%\Downloads\ffmpeg\bin\ffmpeg.exe
 set PYTHON=%USERPROFILE%\AppData\Local\Programs\Python\Python37\python.exe
 
-@REM ��������̏ꍇ�͎g����������
+@REM 引数が空の場合は使い方説明へ
 if "%1"=="" goto USAGE
 
-@REM �J�����g�f�B���N�g�������g�̈ʒu��
+@REM カレントディレクトリを自身の位置へ
 cd /d %~dp0
 
-@REM ffmpeg.exe�̑��݃`�F�b�N
+@REM ffmpeg.exeの存在チェック
 IF EXIST "%FFMPEG%" GOTO FFMPEGOK
 SET FFMPEG=ffmpeg.exe
 WHERE /Q %FFMPEG%
@@ -24,44 +24,44 @@ IF NOT ERRORLEVEL 1 GOTO FFMPEGOK
 SET CMD=WHERE /R "%USERPROFILE%\Downloads" ffmpeg.exe
 FOR /f "usebackq tokens=*" %%a in (`%CMD%`) DO @SET FFMPEG=%%a
 IF EXIST "%FFMPEG%" GOTO FFMPEGOK
-ECHO ffmpeg.exe ��������܂���B�o�b�`�t�@�C���Ƀt���p�X���L�ڂ��邩�p�X��ʂ��Ă��������B
+ECHO ffmpeg.exe が見つかりません。バッチファイルにフルパスを記載するかパスを通してください。
 GOTO ENDOFBATCH
 :FFMPEGOK
 
-@REM python.exe�̑��݃`�F�b�N
+@REM python.exeの存在チェック
 IF EXIST "%PYTHON%" GOTO PYTHONOK
 SET PYTHON=python.exe
 WHERE /Q %PYTHON%
 IF NOT ERRORLEVEL 1 GOTO PYTHONOK
-ECHO python.exe ��������܂���B�o�b�`�t�@�C���Ƀt���p�X���L�ڂ��邩�A�p�X��ʂ��Ă��������B
+ECHO python.exe が見つかりません。バッチファイルにフルパスを記載するか、パスを通してください。
 GOTO ENDOFBATCH
 :PYTHONOK
 
-@REM �O��̍�ƃt�@�C���̍폜
+@REM 前回の作業ファイルの削除
 IF NOT EXIST "work\000001.png" GOTO START
 DEL /F /Q work\*.png
 
 :START
-@REM ffmpeg�̃p�����[�^�ɂ���
-@REM  -r : 1�b�����艽���܂Ńt���[���������邩�B�ړ��ʂ̌댟�m������Ƃ��͒l��傫�����Ă��������B
-@REM       �f�t�H���g5�A�ő�30�ł��B���܂�傫������Ɖ�͂Ɏ��Ԃ�������܂��B
-@REM -ss : �J�n����(�b)
-@REM  -t : �I������(�b)
+@REM ffmpegのパラメータについて
+@REM  -r : 1秒あたり何枚までフレーム分解するか。移動量の誤検知があるときは値を大きくしてください。
+@REM       デフォルト5、最大30です。あまり大きくすると解析に時間がかかります。
+@REM -ss : 開始時間(秒)
+@REM  -t : 終了時間(秒)
 
-@REM ffmpeg�ɂ�铮�恨�Î~�敪��
+@REM ffmpegによる動画→静止画分解
 %FFMPEG% -i %1 -ss 0 -r 5 -f image2 work\%%06d.png
 
-@REM �Î~��̉�͂ƌ���
+@REM 静止画の解析と結合
 %PYTHON% movie2map.py
 
-@REM �o�̓t�@�C���̃��l�[��
+@REM 出力ファイルのリネーム
 RENAME map.png %~nx1.png
 
 GOTO ENDOFBATCH
 
 :USAGE
 ECHO.
-ECHO �g����(Usage) : movie2map.bat [FILENAME]
+ECHO 使い方(Usage) : movie2map.bat [FILENAME]
 
 :ENDOFBATCH
 ENDLOCAL
